82ae8ef1125a73380250933bd4d5e51d15d95811
Johan Glad <johan.glad.johanglad@hotmail.com>
2024-03-24 14:25:08 +0100

    CSS responsiv text

--- src/components/DishComponent.tsx
+++ src/components/DishComponent.tsx
@@ -22,12 +22,17 @@ export default DishComponent;
 const DishContainer = styled.div`
   cursor: pointer;
   margin-bottom: 20px;
+  width: 250px;
+  @media (max-width: 768px) {
+    width: calc(50% - 10px);
+    margin-bottom: 20px;
+  }
 `;
 
 const ImageContainer = styled.div`
   position: relative;
-  width: 250px;
-  height: 250px;
+  width: 100%;
+  height: 100%;
 `;
 
 const DishImage = styled.img`
@@ -41,9 +46,28 @@ const TitleOverlay = styled.div`
   position: absolute;
   bottom: 0;
   width: 100%;
+  height: 20%;
   background-color: rgba(0, 0, 0, 0.5);
   color: white;
-  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   border-bottom-left-radius: 20px;
   border-bottom-right-radius: 20px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  line-height: 1.2em;
+  font-size: calc(2.5vw + 2.5vh + 0.5vmin);
+
+  @media (max-width: 768px) {
+    font-size: 2.5vw;
+  }
+
+  @media (min-width: 769px) {
+    font-size: 1.5vw;
+  }
+
+  @media (min-width: 1024px) {
+    font-size: 16px;
+  }
 `;
--- src/components/MainDishComponent.tsx
+++ src/components/MainDishComponent.tsx
@@ -1,10 +1,11 @@
 import { useState, useEffect } from "react";
 import DishComponent from "./DishComponent";
 import { MainDish } from "../Models/MainDish";
-//import styled from "styled-components";
+import styled from "styled-components";
 
 export const MainDishComponent = () => {
   const [mainDish, setMainDish] = useState<MainDish[]>();
+
   useEffect(() => {
     fetch(`https://iths-2024-recept-grupp5-o9n268.reky.se/recipes`)
       .then((res) => res.json())
@@ -13,5 +14,23 @@ export const MainDishComponent = () => {
         setMainDish(data);
       });
   }, []);
-  return mainDish?.map((dish) => <DishComponent dish={dish} />);
+
+  return (
+    <DishesContainer>
+      {mainDish?.map((dish) => (
+        <DishComponent dish={dish} />
+      ))}
+    </DishesContainer>
+  );
 };
+
+const DishesContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+  justify-content: center;
+
+  @media (max-width: 768px) {
+    justify-content: space-between;
+  }
+`;
