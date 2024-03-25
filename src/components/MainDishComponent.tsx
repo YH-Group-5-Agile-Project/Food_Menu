3f2a5607ee4b89e5c148e3cb082163c9f4c6aa84
Isac Rosenberg <isac.a.rosenberg@gmail.com>
2024-03-25 13:31:07 +0100

    DbService

--- src/components/MainDishComponent.tsx
+++ src/components/MainDishComponent.tsx
@@ -1,20 +1,21 @@
 import { useState, useEffect } from "react";
 import DishComponent from "./DishComponent";
 import { MainDish } from "../Models/MainDish";
+import { GetAllMainDishes } from "../services/DbService";
 
 
 export const MainDishComponent = () => {
-    const [mainDish, setMainDish] = useState<MainDish[]>();
-    useEffect(() => {
-        fetch(`https://iths-2024-recept-grupp5-o9n268.reky.se/recipes`)
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data);
-            setMainDish(data)
-        });
+    // const [mainDish, setMainDish] = useState<MainDish[]>();
+    // useEffect(() => {
+    //     fetch(`https://iths-2024-recept-grupp5-o9n268.reky.se/recipes`)
+    //     .then((res) => res.json())
+    //     .then((data) => {
+    //         console.log(data);
+    //         setMainDish(data)
+    //     });
             
-    }, [])
-    
+    // }, [])
+    let mainDish = GetAllMainDishes()
 
  return (mainDish?.map(dish => <DishComponent dish={dish}/>))
     
---
+++ src/services/DbService.ts
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+import { MainDish } from "../Models/MainDish";
+
+
+export const GetAllMainDishes = () => {
+
+const [mainDish, setMainDish] = useState<MainDish[]>();
+    useEffect(() => {
+        fetch(`https://iths-2024-recept-grupp5-o9n268.reky.se/recipes`)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+            setMainDish(data)
+        });
+            
+    }, [])
+    return mainDish
+}
+
