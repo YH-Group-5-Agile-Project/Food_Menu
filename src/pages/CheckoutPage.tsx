737f99acadd5dd166947f3ce5f8eee3b80c20a6e
MikeBikeX <anttila_micke@hotmail.com>
2024-04-02 19:10:27 +0200

    Update CheckOutPage to display the Order in the Cart.

--- src/pages/CheckoutPage.tsx
+++ src/pages/CheckoutPage.tsx
@@ -1,13 +1,26 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { Cart } from "../Models/Cart";
+import { GetCart } from "../services/CartService";
 
 const CheckoutPage = () => {
   let navigate = useNavigate();
+  const [cart, setCart] = useState<Cart>(null);
+  // Load
+  setCart(GetCart());
   return (
     <div>
       <h1>Min beställning</h1>
       <p>Här kommer du att se alla val du gjort</p>
       <p>och möjlighet att ta bort eventuellt val.</p>
+      <ul>
+        {cart.OrderList.map(order => (
+          <li key={order.id}>
+            Main: {order.main.title}
+          </li>
+        ))}
+      </ul>
       <button onClick={() => navigate(-1)}>Tillbaka</button>
       <Link to="/orderConfirmation">
         <button>Beställ</button>
