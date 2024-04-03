3a0243970269253ad5635ac83424c30c17788700
BFGRSairum <robin.klasson@outlook.com>
2024-04-03 11:18:04 +0200

    React fix

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import { MainDishComponent } from "../components/MainDishComponent";
+import { Navbar } from "../components/NavbarComponent";
+
+
+
+
+const MenuPage = () => {
+  return (
+    <>
+    <Navbar currentPage="menu" />
+    
+  
+      <div>
+        <MainDishComponent dishType="mainDish" />
+      </div>
+
+      <div>
+        <Link to="/order">
+          <button>Min beställning</button>
+        </Link>
+        <Link to="/sides">
+          <button>Nästa steg</button>
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default MenuPage;
