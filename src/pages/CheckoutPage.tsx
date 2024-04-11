8ac5c46258859a1b3744c76ae38f8f356f9d43ba
Johan Glad <johan.glad.johanglad@hotmail.com>
2024-04-11 20:16:25 +0200

    CheckoutComponent är ny. CheckoutPage är ändrad. Nu finns en fungerande Checkout.

--- src/pages/CheckoutPage.tsx
+++ src/pages/CheckoutPage.tsx
@@ -1,35 +1,13 @@
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { Cart } from "../Models/Cart";
-import { GetCart, ResetCart } from "../services/CartService";
+import CheckoutComponent from "../components/CheckoutComponent";
 import { Navbar } from "../components/NavbarComponent";
-import { CartComponent } from "../components/CartComponent";
 
 const CheckoutPage = () => {
-  let navigate = useNavigate();
-  const [cart, setCart] = useState<Cart>({
-    id: 0,
-    OrderList: [],
-    TotalCost: 0,
-  });
-  // Load
-  useEffect(() => {
-    setCart(GetCart());
-  }, []); // render only first time
   return (
     <>
-        <Navbar currentPage="checkout" />
-          <CartComponent/>
-          <div>
-            <button onClick={() => navigate(-1)}>Back</button>
-            <button type="button" onClick={ResetCart}>
-              Reset Cart
-            </button>
-          </div>
+      <Navbar currentPage="checkout" />
+      <CheckoutComponent />
     </>
   );
 };
-
-
 
 export default CheckoutPage;
