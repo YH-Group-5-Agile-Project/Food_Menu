546276a5696e52c75d23ecd1e2803fd3b0e5d78b
Isac Rosenberg <isac.a.rosenberg@gmail.com>
2024-04-12 19:54:05 +0200

    TransitionLeft

--- src/components/DishComponent.tsx
+++ src/components/DishComponent.tsx
@@ -56,6 +56,7 @@ const DishComponent = ({
         <DishImage src={dish.imageUrl} alt={dish.title} />
         {!isSelected && <TitleOverlay>{dish.title}</TitleOverlay>}
       </ImageContainer>
+      {/* {isSelected && 
       <ExpandedDish selected={isSelected}>
         <TextContainer selected={isSelected}>
           <DishTitle>{dish.title}</DishTitle>
@@ -71,6 +72,7 @@ const DishComponent = ({
         </TextContainer>
         <StyledButton onClick={handleAddToCartClick}>Add to Cart</StyledButton>
       </ExpandedDish>
+      } */}
       {isPopupOpen && (
         <AddToCartPopup dish={dish} onClose={() => setIsPopupOpen(false)} />
       )}
@@ -81,11 +83,9 @@ const DishComponent = ({
 export default DishComponent;
 
 const DishContainer = styled.div<FoodProps>`
-  position: relative;
-  display: flex;
   justify-content: center;
   cursor: pointer;
-  width: calc(33.33% - 20px);
+ 
   margin-bottom: 20px;
 
   ${(props) =>
@@ -101,29 +101,33 @@ const DishContainer = styled.div<FoodProps>`
       props.selected &&
       `
       z-index: 1;
-  `}
-  }
+      `}
+    }
+    `;
+    
+const ExpandedDish = styled.div<FoodProps>`
+  width: 100%;
+  grid-column: 1 / -1;
 `;
-
 const ImageContainer = styled.div<FoodProps>`
   position: relative;
   width: 250px;
   height: 250px;
   transition: all 0.3s ease;
-
+  
   @media (max-width: 609px) {
     width: 150px;
     height: 150px;
     ${(props) =>
       props.selected &&
       `
-    width: 100px;
-    height: 100px;
-  `}
-  }
-`;
-
-const DishImage = styled.img`
+      width: 100px;
+      height: 100px;
+      `}
+    }
+    `;
+    
+    const DishImage = styled.img`
   width: 100%;
   height: 100%;
   object-fit: cover;
@@ -152,68 +156,27 @@ const TitleOverlay = styled.div`
   }
 `;
 
-const DishIngredients = styled.div`
-  text-align: left;
-`;
 
 const DishDescription = styled.p`
-  margin: 10px 0;
-  text-align: left;
+margin: 10px 0;
+text-align: left;
 `;
 
 const DishTitle = styled.h2`
-  margin: 10px;
-`;
-
-const DishPrice = styled.h2``;
-
-const ExpandedDish = styled.div<FoodProps>`
-  position: absolute;
-  background-color: #242424;
-  color: rgba(255, 255, 255, 0.87);
-  min-width: 200px;
-  max-width: 100vw;
-  max-height: 0;
-  overflow: hidden;
-  z-index: 1;
-  display: flex;
-  flex-flow: column;
-  align-items: center;
-  border-radius: 20px;
-  opacity: 0;
-  word-wrap: break-word;
-  transition: opacity 0.3s ease-in-out;
-  margin-top: 250px;
-
-  ${(props) =>
-    props.selected &&
-    css`
-      opacity: 1;
-      max-height: 1200px;
-    `}
-
-  @media (prefers-color-scheme: light) {
-    color: #213547;
-    background-color: #ffffff;
-  }
-
-  @media (max-width: 768px) {
-    font-size: 2.5vw;
-    padding: 0 5px 5px 5px;
-    min-width: 150px;
-    max-width: 100%;
-  }
+margin: 10px;
 `;
 
 const TextContainer = styled.div<FoodProps>`
-  width: 100%;
-  margin-bottom: 30px;
-  overflow-wrap: break-word;
-  overflow-y: auto;
 
-  @media (max-width: 768px) {
-    font-size: 2.5vw;
-  }
+@media (max-width: 768px) {
+  font-size: 2.5vw;
+}
+`;
+const DishPrice = styled.h2``;
+
+
+const DishIngredients = styled.div`
+  text-align: left;
 `;
 
 const StyledButton = styled.button`
--- src/components/DishListComponent.tsx
+++ src/components/DishListComponent.tsx
@@ -2,42 +2,130 @@ import { useState } from "react";
 import DishComponent from "./DishComponent";
 import { GetDishes } from "../services/DbService";
 // import { MainDish } from "../Models/MainDish";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+import React from "react";
+import { Dish } from "../Models/Dish";
 
 interface dishInput {
   dishType: string;
 }
 
+interface FoodProps {
+  selected: boolean;
+}
+const transitionTime = 500;
+let timeOutBool = false;
+
 export const DishListComponent = ({ dishType }: dishInput) => {
   const [selectedDish, setSelectedDish] = useState<number | null>(null);
-  const mainDish = GetDishes(dishType);
+  const dishes = GetDishes(dishType);
   const isSideDish = dishType.toLowerCase() === "sidedish" ? true : false;
 
   const HandleClick = (index: number) => {
-    setSelectedDish(index === selectedDish ? null : index);
+    if(index === selectedDish) {
+      timeOutBool = false;
+      setTimeout(() => {
+        setSelectedDish(null);
+      }, transitionTime);
+    }
+    else {
+      timeOutBool = true;
+      setSelectedDish(index);
+    }
   };
+  
+  const getIngredients = (dish: Dish) => {
+    const ingredientsList = dish.ingredients.map((ingredient) => ingredient.name);
+    let ingredients;
+    if (ingredientsList.length > 1) {
+      ingredients =
+        ingredientsList.slice(0, -1).join(", ") +
+        " and " +
+        ingredientsList.slice(-1);
+    } else {
+      ingredients = ingredientsList[0] || "";
+    }
+    return ingredients
+  }
 
   return (
     <DishesContainer>
-      {mainDish?.map((dish, index) => (
-        <DishComponent
+      {dishes?.map((dish, index) => (
+        <>
+          <DishComponent
           key={index}
           dish={dish}
           isSelected={index === selectedDish}
           onClick={() => HandleClick(index)}
           isSideDish={isSideDish}
-        />
-      ))}
+          />
+
+            {index === selectedDish &&
+              <ExpandedDish selected = {timeOutBool}>
+                <TextContainer>
+                  <DishTitle>{dish.title}</DishTitle>
+                  <DishDescription>
+                    <DishPrice>£{dish.price}</DishPrice>
+                    <strong>Description: </strong>
+                    {dish.description}
+                  </DishDescription>
+                  <DishIngredients>
+                    <strong>Ingredients: </strong>
+                    {getIngredients(dish)}.
+                  </DishIngredients>
+                </TextContainer>
+                <StyledButton >Add</StyledButton>
+              </ExpandedDish>
+            }
+          </>
+        ))}
     </DishesContainer>
   );
 };
 
+
+
+const ExpandAnimation = keyframes`
+  0% {
+    max-height: 0;
+    opacity: 0;
+  }
+  25% {
+    opacity: 0;
+  }
+  100% {
+    max-height: 300px;
+    opacity: 1;
+  }
+`
+const CloseAnimation = keyframes`
+  0% {
+    max-height: 300px;
+    opacity: 1;
+  }
+  25% {
+    opacity: 0;
+  }
+  100% {
+    max-height: 0px;
+    opacity: 0;
+  }
+`
+
+const ExpandedDish = styled.div<FoodProps>`
+  width: 100%;
+  grid-column: 1 / -1;
+  grid-row: auto;
+  max-height: ${(prop) => (prop.selected ? '300px' : '0')};
+  transition: max-height ${transitionTime}ms;
+`
+/* animation-name: ${(prop) => (prop.selected ? ExpandAnimation : CloseAnimation)};
+  animation-duration: ${transitionTime / 1000}s; */
 const DishesContainer = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 20px;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+  grid-auto-flow: dense;
   width: 880px;
-  justify-content: center;
 
   @media (max-width: 949px) {
     width: 560px;
@@ -51,3 +139,42 @@ const DishesContainer = styled.div`
     margin: auto;
   }
 `;
+
+const DishDescription = styled.p`
+  margin: 10px 0;
+  text-align: left;
+`;
+
+const DishTitle = styled.h2`
+  margin: 10px;
+`;
+
+const TextContainer = styled.div`
+
+  @media (max-width: 768px) {
+    font-size: 2.5vw;
+  }
+`;
+
+const DishPrice = styled.h2``;
+
+
+const DishIngredients = styled.div`
+  text-align: left;
+`;
+
+const StyledButton = styled.button`
+  margin: 20px;
+`;
+
+const MiddleDiv = styled.div<FoodProps>`
+  
+`
+{/*  */}
+          {/* <button onClick={handleAddToCartClick}>Add to Cart</button> */}
+/* 
+          {!MiddleDivRendered &&
+            <MiddleDiv selected={selectedDish !== null}>
+            </MiddleDiv>
+            }
+            {MiddleDivRendered = true} */
