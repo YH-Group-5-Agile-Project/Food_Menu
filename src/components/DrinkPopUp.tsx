4d591398764b19c6bc188337b552435cfe40e54b
Johan Glad <johan.glad.johanglad@hotmail.com>
2024-04-16 00:31:32 +0200

    DrinkPage och DrinkComponent uppdaterad. DrinkPopUp är en ny komponent som visas när man klickar på en drink.

---
+++ src/components/DrinkPopUp.tsx
@@ -0,0 +1,63 @@
+import styled from "styled-components";
+import { SendDrinkToCart } from "../services/CartService";
+
+export type Drink = {
+  id: string;
+  name: string;
+  alcoholic: boolean;
+  imgUrl: string;
+  ingredients: string[];
+  price: number;
+};
+
+interface DrinkPopUpProps {
+  drink: Drink;
+  onClose: () => void;
+}
+
+const DrinkPopUp = ({ drink, onClose }: DrinkPopUpProps) => {
+  drink.price = 189;
+
+  const handleAddToCartClick = () => {
+    SendDrinkToCart(drink);
+    onClose();
+  };
+
+  return (
+    <PopUpContainer>
+      <PopUpContent onClick={(e) => e.stopPropagation()}>
+        <h2>{drink.name}</h2>
+        <img src={drink.imgUrl} alt={drink.name} style={{ width: "70%" }} />
+        <p>Ingredients: {drink.ingredients.join(", ")}</p>
+        <p>{drink.alcoholic ? "" : "Non-alcholic"}</p>
+        <p>Price: {drink.price} SEK</p>
+        <button onClick={handleAddToCartClick}>Add to Cart</button>
+        <button onClick={onClose}>Close</button>
+      </PopUpContent>
+    </PopUpContainer>
+  );
+};
+
+export default DrinkPopUp;
+
+const PopUpContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 2;
+`;
+
+const PopUpContent = styled.div`
+  background-color: white;
+  padding: 20px;
+  border-radius: 15px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  width: 70%;
+  max-width: 500px;
+`;
--- src/pages/DrinkPage.tsx
+++ src/pages/DrinkPage.tsx
@@ -1,8 +1,8 @@
 import { GetDrink } from "../services/DbService";
 import DrinkComponent from "../components/DrinkComponent";
+import { styled } from "styled-components";
 
 const DrinkPage = () => {
-
   let drinkListIDs = [
     "12768",
     "12618",
@@ -23,20 +23,35 @@ const DrinkPage = () => {
   });
 
   return (
-    <>
-      <div>
-        <h1>Drinkar</h1>
-
-        <div>
-          <div>
-            {drinkList.map((drink) => (
-              <DrinkComponent key={drink?.id} drink={drink} />
-            ))}
-          </div>
-        </div>
-      </div>
-    </>
+    <DrinksContainer>
+      {drinkList
+        .filter((drink) => drink !== null)
+        .map((drink) => (
+          <DrinkComponent key={drink.id} drink={drink} />
+        ))}
+    </DrinksContainer>
   );
 };
 
 export default DrinkPage;
+
+const DrinksContainer = styled.div`
+  width: 900px;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 15px;
+  justify-content: center;
+  margin: auto;
+
+  @media (max-width: 949px) {
+    width: 560px;
+    gap: 20px;
+    margin: auto;
+  }
+
+  @media (max-width: 549px) {
+    width: 360px;
+    gap: 10px;
+    margin: auto;
+  }
+`;
