62bb0817208e95d4b05d170b5a5810ef96b2da17
Jonas395 <jonasgunnarsson99@gmail.com>
2024-04-24 15:24:05 +0200

    Functional pick other drink

--- src/components/DrinkComponentAlt.tsx
+++ src/components/DrinkComponentAlt.tsx
@@ -1,49 +1,50 @@
-import { useState } from "react";
 import styled from "styled-components";
-import DrinkPopUp from "./DrinkPopUp";
 import { DrinkQuery } from "../services/DbService";
+import { Drink } from "../Models/Drink";
 
 interface DrinkComponentProps {
   drinkId: string;
+  sendToCart: (drink?: Drink) => void;
 }
 
-const DrinkComponent = ({ drinkId }: DrinkComponentProps) => {
-  const [isPopUpOpen, setPopUpOpen] = useState(false);
-
-  const togglePopUp = () => setPopUpOpen(!isPopUpOpen);
+const DrinkComponentAlt = ({ drinkId, sendToCart }: DrinkComponentProps) => {
   const { data, isLoading, error } = DrinkQuery(drinkId);
-
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
 
-  let drink = data;
   return (
-    <DrinkContainer onClick={togglePopUp}>
+    <DrinkContainer>
       <ImageContainer>
         <DrinkImage src={data?.imgUrl} alt={data?.name} />
-        <TitleOverlay>{data?.name}</TitleOverlay>
+        <TitleOverlay>{data?.name}</TitleOverlay> 
       </ImageContainer>
-      {isPopUpOpen && <DrinkPopUp drink={drink} onClose={togglePopUp} />}
+      <DrinkDescription>{data?.ingredients.join(", ")}. {data?.alcoholic ? "" : "Non-alcholic"}.</DrinkDescription>
+      <button onClick={() => sendToCart(data)}> Add {data?.name} to menu</button>
     </DrinkContainer>
   );
 };
 
+const DrinkDescription = styled.div`
+margin-top: 5px;
+padding: 10px;
+`
 const DrinkContainer = styled.div`
   position: relative;
   display: flex;
+  flex-wrap: wrap;
   justify-content: center;
   cursor: pointer;
-  margin-bottom: 5px;
+  margin-bottom: 6rem;
 `;
 
 const ImageContainer = styled.div`
   position: relative;
-  width: 250px;
-  height: 250px;
+  width: 220px;
+  height: 220px;
 
-  @media (max-width: 949px) {
-    width: 150px;
-    height: 150px;
+  @media (max-width: 849px) {
+    width: 130px;
+    height: 130px;
   }
 `;
 
@@ -76,4 +77,4 @@ const TitleOverlay = styled.div`
   }
 `;
 
-export default DrinkComponent;
+export default DrinkComponentAlt;
